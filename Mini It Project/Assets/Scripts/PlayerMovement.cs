0750dd400c3dcfb2a97839781d41f04b58d1b810
ZhiXuan1234 <164007596+ZhiXuan1234@users.noreply.github.com>
2024-03-23 17:30:07 +0800

    Update PlayerMovement.cs

--- Mini It Project/Assets/Scripts/PlayerMovement.cs
+++ Mini It Project/Assets/Scripts/PlayerMovement.cs
@@ -12,5 +12,12 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+
+        if(Input.GetKey(KeyCode.Space))
+            body.velocity = new Vector2(body.velocity.x, speed);
     }
+
+
+
+
 }
