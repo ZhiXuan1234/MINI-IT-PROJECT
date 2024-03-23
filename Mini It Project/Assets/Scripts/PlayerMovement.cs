a501e025f8829474ccddb0b8970ef9ce916f5222
ZhiXuan1234 <164007596+ZhiXuan1234@users.noreply.github.com>
2024-03-23 17:23:59 +0800

    changes

--- Mini It Project/Assets/Scripts/PlayerMovement.cs
+++ Mini It Project/Assets/Scripts/PlayerMovement.cs
@@ -2,9 +2,15 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody2D body; 
-
-
-
-        
+    [SerializeField] private float speed;
+    private Rigidbody2D body;
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
     }
+
+    private void Update()
+    {
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+    }
+}
