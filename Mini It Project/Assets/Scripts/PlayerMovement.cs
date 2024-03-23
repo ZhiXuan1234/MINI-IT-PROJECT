59da1bbbaaecc654ee8551a843da002d4980234f
ZhiXuan1234 <164007596+ZhiXuan1234@users.noreply.github.com>
2024-03-23 18:26:35 +0800

    changes

--- Mini It Project/Assets/Scripts/PlayerMovement.cs
+++ Mini It Project/Assets/Scripts/PlayerMovement.cs
@@ -4,17 +4,32 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
     private Rigidbody2D body;
+    private Animator anim;
+
+
+
     private void Awake()
     {
+        //Grab references for your rigidbody and animator from object
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
 
-        if(Input.GetKey(KeyCode.Space))
+        //Flip player when moving left
+        if (horizontalInput > 0.01f)
+            transform.localScale = Vector3.one;
+        else if (horizontalInput < 0.01f)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        if (Input.GetKey(KeyCode.Space))
             body.velocity = new Vector2(body.velocity.x, speed);
+
+        //Set animator parameters
     }
 
 
