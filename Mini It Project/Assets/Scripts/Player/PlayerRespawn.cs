8c0b53517a08ac64c1923c4816feb673b6939d5a
ZhiXuan1234 <164007596+ZhiXuan1234@users.noreply.github.com>
2024-03-29 21:11:14 +0800

    9

---
+++ Mini It Project/Assets/Scripts/Player/PlayerRespawn.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class PlayerRespawn : MonoBehaviour
+{
+    [SerializeField] private AudioClip checkpoint;
+    private Transform currentCheckpoint;
+    private Health playerHealth;
+
+    private void Awake()
+    {
+        playerHealth = GetComponent<Health>();
+    }
+
+    public void CheckRespawn()
+    {
+        //Check if checkpoint available
+        if (currentCheckpoint == null)
+        {
+            //Show game over screen
+
+            return; //Dont execute this function
+        
+        }
+        playerHealth.Respawn();// Restore player health and reset animation
+        transform.position = currentCheckpoint.position; //Move player to checkpoint position
+         
+
+        //move the camera back to the player
+        // Camera.main.GetComponent<CameraController>().MoveToNewRoom(currentCheckpoint.parent);
+    }
+
+    //Activate checkpoints
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Checkpoint")
+        {
+            currentCheckpoint = collision.transform; //Store the checkpoint that we activated as the current one
+            SoundManager.instance.PlaySound(checkpoint);
+            collision.GetComponent<Collider2D>().enabled = false; //Deactivate checkpoint collider
+            collision.GetComponent<Animator>().SetTrigger("appear"); //Triger checkpoint animation
+        
+        }
+    }
+}
+    
+
+    
