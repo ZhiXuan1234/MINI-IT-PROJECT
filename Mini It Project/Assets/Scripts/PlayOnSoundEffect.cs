9673abd139509f07500afe1442dc373ef6a64444
Jannie0717 <145191007+Jannie0717@users.noreply.github.com>
2024-04-15 21:25:36 +0800

    changes

--- Mini It Project/Assets/Scripts/PlayOnSoundEffect.cs
+++ Mini It Project/Assets/Scripts/PlayOnSoundEffect.cs
@@ -7,13 +7,22 @@ public class PlayOnSoundEffect : MonoBehaviour
     AudioSource source;
     Collider2D soundTrigger;
 
-    private void Awake()
+    void Awake()
     {
         source = GetComponent<AudioSource>();
         soundTrigger = GetComponent<Collider2D>();
     }
 
-    void OnTriggerEnter2D(Collider2D collider){
-        source.Play();
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            source.Play();
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        //source.Play();
     }
 }
