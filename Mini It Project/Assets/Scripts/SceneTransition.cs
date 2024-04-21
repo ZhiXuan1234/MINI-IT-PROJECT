df52e393bbc7cc7c7fba4a393ac32000c9486ed6
Jannie0717 <145191007+Jannie0717@users.noreply.github.com>
2024-04-21 21:00:50 +0800

    changes

---
+++ Mini It Project/Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    public static SceneTransition instance;
+    [SerializeField] Animator transitionAnim;
+
+
+    public void NextLevel()
+    {
+        StartCoroutine(LoadLevel());
+    }
+
+    IEnumerator LoadLevel()
+    {
+        transitionAnim.SetTrigger("End");
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+        transitionAnim.SetTrigger("Start");
+    }
+}
